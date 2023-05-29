95609da9b93caedd74f2c84198b8d35abd8d2942
ABBNojGau <Nojus.gaupsas@abbgymnasiet.se>
2023-05-29 22:57:14 +0200

    df

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@nuxtjs/google-adsense"],
-  "google-adsense": {
-    id: "ca-pub-1449694773096456",
-  },
-};
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
+});
